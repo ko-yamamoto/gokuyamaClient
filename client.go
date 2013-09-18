30c6e023cc12100ca0f83311ba24b2803f6cd907
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-09-18 23:27:54 +0900

    add comment

--- client.go
+++ client.go
@@ -1,3 +1,5 @@
+// Package client provides GokuyamaClient for connecting to okuyama servers,
+// setting key-value and getting value.
 package client
 
 import (
@@ -8,22 +10,26 @@ import (
 	"strings"
 )
 
+// GokuyamaClient has a connection with okuyama
 type GokuyamaClient struct {
 	conn net.Conn
 }
 
+// connect to a okuyama master node
 func (gc *GokuyamaClient) Connect(hostname string, portNo int) error {
 	var err error
 	gc.conn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", hostname, portNo))
 	return err
 }
 
+// close connection to a okuyama master node
 func (gc *GokuyamaClient) Close() error {
 	var err error
 	err = gc.conn.Close()
 	return err
 }
 
+// set a key-value to okuyama
 func (gc *GokuyamaClient) SetValue(key string, value string) bool {
 
 	fmt.Fprintf(gc.conn, fmt.Sprintf("1,%s,(B),0,%s\n",
@@ -40,6 +46,7 @@ func (gc *GokuyamaClient) SetValue(key string, value string) bool {
 
 }
 
+// set a key-value with a tag to okuyama
 func (gc *GokuyamaClient) SetValueWithTag(key string, value string, tag string) bool {
 
 	fmt.Fprintf(gc.conn, fmt.Sprintf("1,%s,%s,0,%s\n",
@@ -57,6 +64,7 @@ func (gc *GokuyamaClient) SetValueWithTag(key string, value string, tag string) 
 
 }
 
+// get value by a key from okuyama
 func (gc *GokuyamaClient) GetValue(key string) (string, error) {
 
 	fmt.Fprintf(gc.conn, fmt.Sprintf("2,%s\n", base64.StdEncoding.EncodeToString([]byte(key))))
@@ -80,9 +88,10 @@ func (gc *GokuyamaClient) GetValue(key string) (string, error) {
 	}
 }
 
+// get keys by a tag from okuyama
 func (gc *GokuyamaClient) GetKeysByTag(tag string) ([]string, error) {
 
-	fmt.Fprintf(gc.conn, fmt.Sprintf("3,%s,True\n", base64.StdEncoding.EncodeToString([]byte(tag))))
+	fmt.Fprintf(gc.conn, fmt.Sprintf("3,%s,False\n", base64.StdEncoding.EncodeToString([]byte(tag))))
 
 	ret, err := bufio.NewReader(gc.conn).ReadString('\n')
 	if ret == "" {
